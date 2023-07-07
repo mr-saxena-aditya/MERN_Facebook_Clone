f8e7d13fde5ae5bb7e63fa1827d15221b397d5f6
mr-saxena-aditya <mr.saxena.aditya@gmail.com>
2023-07-07 03:07:41 -0400

    planning CORS Options

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,51 @@
-const express = require('express');
+const express = require('express'); // Import the Express framework
 
-const app = express();
+const cors = require('cors'); // Import the CORS middleware
+
+const app = express(); // Create an instance of the Express application
+
+let allowed = ['http://localhost:3000', 'some other link']; // Create an array of allowed origins
+
+// Create a middleware function to handle CORS options
+function option(req, res) {
+    let tmp;
+    let origin = req.header('Origin'); // Get the Origin header from the request
+    if (allowed.indexOf(origin) !== -1) { // If the Origin header is in the list of allowed origins
+        tmp = true;
+        optionSuccessStatus = 200; // Set the success status for CORS options
+    } else {
+        tmp = {
+            status: 403,
+            message: 'Access Denied'
+        };
+    }
+
+    res(null, tmp);
+
+    return tmp;
+}
+
+const options = {
+    origin: 'http://localhost:3000', // Specify the allowed origin
+    useSuccessStatus: 200, // Allow successful CORS requests
+    credentials: true // Allow credentials to be included in CORS requests
+}
+
+app.use(cors(options)); // Apply the CORS middleware to the Express app
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World: Welcome from the server!');
+    res.send('Hello World: Welcome from the server!'); // Handle GET requests to the root route
 });
 
 app.get('/about', (req, res) => {
-    res.send('About : Welcome from the server!');
+    res.send('About : Welcome from the server!'); // Handle GET requests to the /about route
 });
 
 app.get('/contact', (req, res) => {
-    res.send('Contact : Welcome from the server!');
+    res.send('Contact : Welcome from the server!'); // Handle GET requests to the /contact route
 });
 
 app.listen(8000, () => {
-    console.log('Server is running on port 8000');
-})
+    console.log('Server is running on port 8000'); // Start the server and listen on port 8000
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
-import { LiveVideo } from "./svg"; 
+import { LiveVideo } from "./svg"; // Importing the LiveVideo component from "./svg"
+
 function App() {
+
+  const get=async () => {
+    const res = await fetch('http://localhost/8000'); // Sending a GET request to 'http://localhost/8000'
+    console.log(res); // Logging the response object
+  };
+
   return (
     <div>
-      welcome to frontend
+      welcome to frontend // Rendering a text message
     </div>
   );
   
 }
 
-export default App;
+export default App; // Exporting the App component as the default export
