d2fef17ba41fa12b536f3d23a9b4e62fc3d506ee
mr-saxena-aditya <mr.saxena.aditya@gmail.com>
2023-07-07 03:08:51 -0400

    planning CORS Options and allowing everything

--- backend/server.js
+++ backend/server.js
@@ -6,33 +6,7 @@ const app = express(); // Create an instance of the Express application
 
 let allowed = ['http://localhost:3000', 'some other link']; // Create an array of allowed origins
 
-// Create a middleware function to handle CORS options
-function option(req, res) {
-    let tmp;
-    let origin = req.header('Origin'); // Get the Origin header from the request
-    if (allowed.indexOf(origin) !== -1) { // If the Origin header is in the list of allowed origins
-        tmp = true;
-        optionSuccessStatus = 200; // Set the success status for CORS options
-    } else {
-        tmp = {
-            status: 403,
-            message: 'Access Denied'
-        };
-    }
-
-    res(null, tmp);
-
-    return tmp;
-}
-
-const options = {
-    origin: 'http://localhost:3000', // Specify the allowed origin
-    useSuccessStatus: 200, // Allow successful CORS requests
-    credentials: true // Allow credentials to be included in CORS requests
-}
-
-app.use(cors(options)); // Apply the CORS middleware to the Express app
-
+app.use(cors());// Use the CORS middleware
 
 app.get('/', (req, res) => {
     res.send('Hello World: Welcome from the server!'); // Handle GET requests to the root route
