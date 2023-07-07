3cfa7951be0ddec2555f64fddc3d41439576c41c
mr-saxena-aditya <mr.saxena.aditya@gmail.com>
2023-07-07 17:01:10 -0400

    added routing and controllers

---
+++ backend/controllers/user.js
@@ -0,0 +1,5 @@
+// Define the home function for handling the user home route
+exports.home = (req, res) => {
+    res.send("User Home: Welcome from the server");
+  };
+  
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,13 +1,12 @@
-/*
-This code defines a router that handles routes related to the '/user' endpoint.
-*/
+// Import the Express framework
+const express = require('express');
+// Create a new router instance
+const router = express.Router();
+// Import the home function from the ../controllers/user module
+const { home } = require("../controllers/user");
 
-const express = require('express'); // Import the Express framework
-const router = express.Router(); // Create a new router instance
+// Handle GET requests to the '/user' route using the home controller
+router.get("/user", home);
 
-// Handle GET requests to the '/user' route
-router.get("/user", (req, res) => {
-    res.send('User: Welcome from the server!'); // Send a response with the message "User: Welcome from the server!"
-});
-
-module.exports = router; // Export the router to be used in other modules
+// Export the router to be used in other modules
+module.exports = router;
