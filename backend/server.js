c06dffe33e8d5d13cf86a5a22ec92906e05c431c
mr-saxena-aditya <mr.saxena.aditya@gmail.com>
2023-07-08 15:47:16 -0400

    creating a dotenv file to hold environmental variables

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 /**
  * This code sets up an Express server that handles various routes and uses CORS middleware to allow cross-origin requests.
+ * It connects to a MongoDB database and listens on a specified port.
  */
 
 // Import the necessary modules
@@ -7,14 +8,15 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const { readdirSync } = require('fs');
-const dotenv = require('dotenv');   // Load the.env file
-dotenv.config(); // Read the.env file
+const dotenv = require('dotenv');
+
+dotenv.config(); // Load the .env file
 
 const app = express(); // Create an instance of the Express application
 
 app.use(cors()); // Use the CORS middleware to allow cross-origin requests
 
-// Read all the files in the 'routes' folder and map them to corresponding routes
+// Register routes
 readdirSync('./routes/').map((route) => {
     const routePath = "./routes/" + route;
     app.use("/", require(routePath));
@@ -25,12 +27,13 @@ mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true, // Use newUrlParser to connect to the MongoDB database
     useUnifiedTopology: true // Use useUnifiedTopology to connect to the MongoDB database
 })
-.then(() => { console.log("Connected to MongoDB") }) // If the connection is successful then log the success message to the console and exit.
-.catch((err) => { console.log(err); }); // If the connection is not successful then log the error to the console and exit.
+.then(() => { console.log("Connected to MongoDB") }) // If the connection is successful then log the success message to the console
+.catch((err) => { console.log(err); }); // If the connection is not successful then log the error to the console
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000; // Set the port number
 
+// Start the server and listen on the specified port
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`); // Start the server and listen on the specified port
+    console.log(`Server is running on port ${port}`);
     console.log('Press Ctrl+C to stop the server');
 });
