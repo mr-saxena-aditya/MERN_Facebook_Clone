14ae6467dd1a610ca2d6e69ae9954d3802df2fb9
mr-saxena-aditya <mr.saxena.aditya@gmail.com>
2023-07-08 15:42:47 -0400

    creating a connection with MongoDB

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@
 // Import the necessary modules
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const { readdirSync } = require('fs');
 const dotenv = require('dotenv');   // Load the.env file
 dotenv.config(); // Read the.env file
@@ -18,6 +19,14 @@ readdirSync('./routes/').map((route) => {
     const routePath = "./routes/" + route;
     app.use("/", require(routePath));
 });
+
+// Connect to the MongoDB database
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true, // Use newUrlParser to connect to the MongoDB database
+    useUnifiedTopology: true // Use useUnifiedTopology to connect to the MongoDB database
+})
+.then(() => { console.log("Connected to MongoDB") }) // If the connection is successful then log the success message to the console and exit.
+.catch((err) => { console.log(err); }); // If the connection is not successful then log the error to the console and exit.
 
 const port = process.env.PORT || 8000;
 
