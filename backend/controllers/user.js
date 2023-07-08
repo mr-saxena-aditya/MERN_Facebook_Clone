86c5052d8b7b3fa8d2a6e944f0032a134b9a1b36
mr-saxena-aditya <mr.saxena.aditya@gmail.com>
2023-07-08 16:05:48 -0400

    testing with JSON Statuses

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,6 @@
 // Define the home function for handling the user home route
 exports.home = (req, res) => {
-    res.json({
+    res.status(200).json({
       message: "Welcome to the API",
       status: 200,
       data: "This is the facebook API",
