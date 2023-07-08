50a2e3086ee90de95f6a053c4c5fea2ce0fb910b
mr-saxena-aditya <mr.saxena.aditya@gmail.com>
2023-07-08 13:36:17 -0400

    creating a dotenv file to hold environmental variables and testing it

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,9 @@ readdirSync('./routes/').map((route) => {
     app.use("/", require(routePath));
 });
 
-app.listen(8000, () => {
-    console.log('Server is running on port 8000'); // Start the server and listen on port 8000
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`); // Start the server and listen on the specified port
+    console.log('Press Ctrl+C to stop the server');
 });
