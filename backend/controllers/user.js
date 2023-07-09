c32754006d8276361d242b0feb870c219509728c
mr-saxena-aditya <mr.saxena.aditya@gmail.com>
2023-07-09 05:07:17 -0400

    testing POST requests with email validation

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,9 @@
 // Import the User model
 const User = require("../models/User");
 
+// Import the validateEmail helper function
+const { validateEmail} = require("../helpers/validation"); 
+
 /**
  * Registers a new user.
  * Endpoint: POST /register
@@ -20,24 +23,29 @@ exports.register = async (req, res) => {
     verified,
   } = req.body;
 
-  try {
-    // Create a new user instance
-    const user = await new User({
-      first_name,
-      last_name,
-      username,
-      email,
-      password,
-      gender,
-      date_of_birth,
-      verified,
-    }).save();
 
-    // Return the user data as JSON response
-    res.json(user);
-  } catch (error) {
-    console.error(error);
-    // If an error occurs, send an error response with 500 status code
-    res.status(500).json({ error: "An error occurred while registering the user." });
+  if (! validateEmail(email)) {
+    return res.status(400).json({ error: "Please enter a valid email address." });
+  } else {
+    try {
+      // Create a new user instance
+      const user = await new User({
+        first_name,
+        last_name,
+        username,
+        email,
+        password,
+        gender,
+        date_of_birth,
+        verified,
+      }).save();
+  
+      // Return the user data as JSON response
+      res.json(user);
+    } catch (error) {
+      console.error(error);
+      // If an error occurs, send an error response with 500 status code
+      res.status(500).json({ error: "An error occurred while registering the user." });
+    }
   }
 };
---
+++ backend/helpers/validation.js
@@ -0,0 +1,22 @@
+/**
+ * Validates an email address.
+ * 
+ * @param {string} email - The email address to validate.
+ * @returns {boolean} - Returns true if the email is valid, false otherwise.
+ */
+exports.validateEmail = (email) => {
+    /**
+     * Regular expression for email validation
+     * ^ asserts the start of the string.
+     * [\w.-]+ matches one or more word characters (letters, digits, or underscores), dots, or hyphens. This represents the local part of the email address before the "@" symbol.
+     * @ matches the "@" symbol.
+     * [a-zA-Z_-]+? matches one or more alphabetical characters (upper or lower case) or underscores. This represents the domain part of the email address after the "@" symbol.
+     * (?:\.[a-zA-Z]{2,})+ is a non-capturing group that matches a dot followed by two or more alphabetical characters. This represents the top-level domain (TLD) part of the email address. The "+" quantifier allows for multiple TLDs separated by dots.
+     * $ asserts the end of the string.
+     */
+    const emailRegex = /^[\w.-]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,})+$/;
+  
+    // Convert email to lowercase and match against the regular expression
+    return emailRegex.test(String(email).toLowerCase());
+  };
+  
