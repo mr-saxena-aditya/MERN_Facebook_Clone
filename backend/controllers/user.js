8492fa929192ea83d07ee42a4126993652d4f38b
mr-saxena-aditya <mr.saxena.aditya@gmail.com>
2023-07-09 15:37:17 -0400

    adding validation for user creation

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,8 +1,5 @@
-// Import the User model
-const User = require("../models/User");
-
-// Import the validateEmail helper function
-const { validateEmail} = require("../helpers/validation"); 
+const User = require("../models/User"); // Import the User model
+const { validateEmail } = require("../helpers/validation"); // Import the validateEmail helper function
 
 /**
  * Registers a new user.
@@ -20,32 +17,63 @@ exports.register = async (req, res) => {
     password,
     gender,
     date_of_birth,
-    verified,
-  } = req.body;
+  } = req.body; // Extract the required data from the request body
 
-
-  if (! validateEmail(email)) {
-    return res.status(400).json({ error: "Please enter a valid email address." });
-  } else {
-    try {
-      // Create a new user instance
-      const user = await new User({
-        first_name,
-        last_name,
-        username,
-        email,
-        password,
-        gender,
-        date_of_birth,
-        verified,
-      }).save();
-  
-      // Return the user data as JSON response
-      res.json(user);
-    } catch (error) {
-      console.error(error);
-      // If an error occurs, send an error response with 500 status code
-      res.status(500).json({ error: "An error occurred while registering the user." });
+  try {
+    // Check if the username is already in use
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+      console.error("The username is already in use. Please try again with a different username.");
+      return res.status(400).json({ error: "The username is already in use. Please try again with a different username." });
     }
+    
+    // Check if the email is already in use
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      console.error("The email address is already in use. Please try again with a different email.");
+      return res.status(400).json({ error: "The email address is already in use. Please try again with a different email." });
+    }
+
+    // Validate the email using the helper function
+    if (!validateEmail(email)) {
+      console.error("Please enter a valid email address.");
+      return res.status(400).json({ error: "Please enter a valid email address." });
+    }
+
+    // Validate the length of first_name and last_name
+    if (first_name.length < 2 || last_name.length < 2) {
+      console.error("First name and last name must be at least 2 characters long.");
+      return res.status(400).json({ error: "First name and last name must be at least 2 characters long." });
+    }
+
+    // Validate the length and strength of the password
+    if (password.length < 10) {
+      console.error("Password must be at least 10 characters long.");
+      return res.status(400).json({ error: "Password must be at least 10 characters long." });
+    }
+
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]+$/;
+    if (!passwordRegex.test(password)) {
+      console.error("Password must contain at least one alphabet, one number, and one special character.");
+      return res.status(400).json({ error: "Password must contain at least one alphabet, one number, and one special character." });
+    }
+
+    // Create a new user instance with the provided data
+    const user = await new User({
+      first_name,
+      last_name,
+      username,
+      email,
+      password,
+      gender,
+      date_of_birth,
+    }).save();
+
+    // Return the user data as a JSON response
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    // If an error occurs while registering the user, send a 500 error response
+    res.status(500).json({ error: "An error occurred while registering the user." });
   }
 };
